a4f809e2783b31ed72f2a30ef530c20f8b349adb
nikoirwin33 <146851989+nikoirwin33@users.noreply.github.com>
2023-11-21 02:19:24 -0500

    changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let bckColors =["lightblue","lightpink"];
 let i = 0;
 setInterval(function(){
   document.body.style.backgroundColor = bckColors [i]; i= (i+1) % bckColors.length;},2000)
-  
+
 const boxes = document.querySelectorAll(".box");
 
 // create function for each box
@@ -10,7 +10,17 @@ boxes.forEach(function (box) {
   let randomNum = Math.floor(Math.random() * 12);
   box.style.order = randomNum;
 });
-// intitate cards and counter
+// intitate varibles for cards and counter
 let firstCard;
 let secondCard;
 let clickCounter = 0;
+
+
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    if (!firstCard && !secondCard) {
+      firstCard = box;
+      box.classList.add("show");
+    }
+  }
+}
