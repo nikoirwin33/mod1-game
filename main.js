c8559e5dc2d461292cf611385117b940fc91259f
nikoirwin33 <146851989+nikoirwin33@users.noreply.github.com>
2023-11-21 01:58:57 -0500

    add cardarray

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ boxes.forEach(function (box) {
   let randomNum = Math.floor(Math.random() * 12);
   box.style.order = randomNum;
 });
+
+const cardArray = [ 
+  { name: "heyarnold", image:"heyarnold1.png", }, 
+  { name: "heyarnold", image: "heyarnold1.png", },
+  { name: "mojo", image: "mojojojo.png", },
+  { name: "mojo", image: "pizza.png", }, 
+  { name: "patrick", image: "patrick1.png", },
+  { name: "patrick", image: "patrick1.png", }, 
+  { name: "rocketpower", image: "rocketpower.png",},
+  { name: "rocketpower", image: "rocketpower.png", },
+  { name: "rugrats", image: "rugrats.png", },
+  { name: "rugrats", image: "rugrats.png", },
+  { name: "spongebob", image: "spongebob1.png", },
+  { name: "spongebob", image: "spongebob1.png", }, 
+  ]; 
